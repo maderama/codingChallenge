03ca7025dba5f2a6a23f9ba62de031335b27899e
Madison Beare <madisonbeare@gmail.com>
2023-11-22 14:51:48 +1000

    remove unused imports

--- programmingTask/src/test/java/ProgrammingTaskTest.java
+++ programmingTask/src/test/java/ProgrammingTaskTest.java
@@ -3,14 +3,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.*;
 import java.util.*;
 
 
